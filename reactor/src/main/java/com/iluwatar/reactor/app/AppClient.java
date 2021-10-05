332db25e33a1c5432efabf14a1cdf96d0730eb28
Yasas <yasasw@uom.lk>
2021-10-05 17:03:01 +0530

    improved code

--- reactor/src/main/java/com/iluwatar/reactor/app/AppClient.java
+++ reactor/src/main/java/com/iluwatar/reactor/app/AppClient.java
@@ -31,10 +31,8 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 
-import javax.net.ssl.SSLServerSocketFactory;
 import javax.net.ssl.SSLSocketFactory;
 
 /**
